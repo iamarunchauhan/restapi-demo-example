ec2ad6a661917094df1a220c0d25780d0c1e6bec
iamarunchauhan <arunchauhan2319@gmail.com>
2023-05-15 12:08:15 +0530

    Added logger statement

--- src/main/java/com/example/restapi/controller/UserController.java
+++ src/main/java/com/example/restapi/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.restapi.controller;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 
+	private static Logger logger = LogManager.getLogger(UserController.class);
+	
 	@GetMapping("/users")
 	public String getUserDetails() {
+		logger.info("Inside users API");
 		return "Users Details are available!";
 	}
 }
